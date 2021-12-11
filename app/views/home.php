7ed19012d9bfcb22b6ac4d4a9f202b8a61ce4f31
João <joaovelez86@gmail.com>
2021-12-11 13:10:00 +0000

    Finalizada a estrutura base de MVC com as respectivas routes da app

---
+++ app/views/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esta é a homepage</title>
+</head>
+
+<body>
+    Esta é a homepage
+</body>
+
+</html>
